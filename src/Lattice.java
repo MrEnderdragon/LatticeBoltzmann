15f9d3f04fdd968f8da8cb20276fef9446a7afaa
RippleSplash <henryzhao@zlww.com>
2021-04-21 11:13:22 -0700

    added a particle flow-tracing system

--- src/Lattice.java
+++ src/Lattice.java
@@ -12,6 +12,8 @@ public class Lattice extends JPanel {
     double[][] macroDense; // p
     double[][][] macroVel; // u
 
+    boolean[][] isPart;
+
     boolean[][] isWall;
     short[][] zouHe; // 0 is none, 1 is pressure, 2 is velocity, 3 is both
     short[][] zouHeOr; // 0 is top, 1 is left, 2 is bottom, 3 is right
@@ -22,11 +24,26 @@ public class Lattice extends JPanel {
     int height;
 
     int c = 1; //lattice speed
-    double t = 4d/6; // relaxation time to equilibrium
+    double t = 5d/6; // relaxation time to equilibrium
     double denseMult = 128;
     double velMult = 128;
     double lim = 1e100;
+    double sqrMult = 0.5;
+    double partTop = 175;
+    double partBot = 75;
 
+    setup curSet;
+    color curCol;
+
+    enum setup {
+        MoveLid,
+        Poiseuille
+    }
+
+    enum color {
+        partVel,
+        denseVel
+    }
 
     private double[][] e = new double[][] {
             {0,0},
@@ -52,76 +69,34 @@ public class Lattice extends JPanel {
             0, 3, 4, 1, 2, 7, 8, 5, 6
     };
 
-    public Lattice(int width, int height) {
+    public Lattice(int width, int height, setup cur, color curColl) {
         this.width = width;
         this.height = height;
+
+        curSet = cur;
+        curCol = curColl;
 
         zouHe = new short[width][height];
         zouHeOr = new short[width][height];
         zhPres = new double[width][height];
         zhVel = new double[width][height][2];
 
-        this.reset();
-
-//        lattice = new double[width][height][9];
-//        latticeStar = new double[width][height][9];
-//        eqDis = new double[width][height][9];
-//        macroDense = new double[width][height];
-//        macroVel = new double[width][height][2];
-//        isWall = new boolean[width][height];
-//        zouHe = new short[width][height];
-//        zouHeOr = new short[width][height];
-//        zhPres = new double[width][height];
-//        zhVel = new double[width][height][2];
-//
-//        for (int i = 0; i < width; i++) {
-//            for (int j = 0; j < height; j++) {
-//                for (int k = 0; k < 9; k++) {
-//                    lattice[i][j][k] = w[k];
-////                    latticeStar[i][j][k] = w[k];
-//                }
-//
-//                macroDense[i][j] = 1;
-//                macroVel[i][j][0] = 0.5;
-//
-//                if(i == 0){
-//                    isWall[i][j] = true;
-//                } else if(i == width-1){
-//                    isWall[i][j] = true;
-//                } else if(j == 0){
-//                    isWall[i][j] = true;
-//                } else if (j == height-1){
-//                    isWall[i][j] = true;
-//                }
-//            }
-//        }
-//
-//        this.width = width;
-//        this.height = height;
-//
-//        stream2();
-////        updateMDMVED();
-//        updateMD();
-//        updateMV();
-//
-//        for (int i = 0; i < width; i++) {
-//            for (int j = 0; j < height; j++) {
-//                macroDense[i][j] = 1;
-//                macroVel[i][j][0] = 0.5;
-//            }
-//        }
-//
-//        updateED();
-//        collide();
+        this.resetWalls();
+        this.resetFlow();
     }
 
-    public void reset () {
+    public void resetFlow () {
         lattice = new double[width][height][9];
         latticeStar = new double[width][height][9];
         eqDis = new double[width][height][9];
         macroDense = new double[width][height];
         macroVel = new double[width][height][2];
-        isWall = new boolean[width][height];
+
+        isPart = new boolean[width][height];
+
+        if(isWall.length != width || isWall[0].length != height){
+            //TODO: add in resizing
+        }
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
@@ -147,24 +122,49 @@ public class Lattice extends JPanel {
             }
         }
 
-        stream2();
+        if(curSet == setup.Poiseuille) {
+            for (int i = 2; i < height-2; i++) {
+                zouHe[1][i] = 3;
+                zouHeOr[1][i] = 1;
+                zhVel[1][i][0] = 1d/2;
+                zhPres[1][i] = 1;
+            }
+
+            for (int i = 2; i < height-2; i++) {
+                zouHe[width-2][i] = 1;
+                zouHeOr[width-2][i] = 3;
+//                lat.zhVel[lat.width-2][i][0] = -1d/7;
+                zhPres[width-2][i] = -0;
+            }
+
+            stream2();
 //        updateMDMVED();
-        updateMD();
-        updateMV();
+            updateMD();
+            updateMV();
 
-        // ~~~~~~~~~~~~~~~~    Poiseuille flow ~~~~~~~~~~~~~~~~
-
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
 //                macroDense[i][j] = (1d-0.2)/width * (width-i);
 //                macroDense[i][j] = 1.3;
-                macroVel[i][j][0] = 0.1;
+                    macroVel[i][j][0] = 0.1;
 //                macroVel[i][j][0] = (0.41-0.21)/width * (width-i) + 0.21;
+                }
+            }
+
+            updateED();
+            collide();
+            moveParts();
+        }else if (curSet == setup.MoveLid){
+            for (int i = 2; i < height-2; i++) {
+                zouHe[1][i] = 2;
+                zouHeOr[1][i] = 1;
+                zhVel[1][i][1] = 1d/10;
             }
         }
+    }
 
-        updateED();
-        collide();
+    public void resetWalls () {
+        isWall = new boolean[width][height];
     }
 
     private static double dot(double[] vect_A, double[] vect_B) {
@@ -382,14 +382,76 @@ public class Lattice extends JPanel {
         }
     }
 
+    public void movePartsOld () {
+        boolean[][] nextPart = new boolean[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if((zouHe[i][j] & 2) != 0){
+                    isPart[i][j] = true;
+                }
+                if(isPart[i][j]) {
+                    double max = 0;
+                    int maxInd = 0;
+                    for (int k = 0; k < 9; k++) {
+                        if (Math.abs(lattice[i][j][k])/w[k] >= max) {
+                            maxInd = k;
+                            max = lattice[i][j][k]/w[k];
+                        }
+                    }
+
+                    if(i + e[maxInd][0] >= 0 && i + e[maxInd][0] < width && j + e[maxInd][1] >= 0 && j + e[maxInd][1] < height){
+                        nextPart[i + (int)e[maxInd][0]][j + (int)e[maxInd][1]] = true;
+                    }
+
+//                    System.out.println(maxInd + "     "  +max);
+                }
+            }
+        }
+        isPart = nextPart;
+    }
+
+    public void moveParts () {
+        boolean[][] nextPart = new boolean[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if((zouHe[i][j] & 2) != 0){
+                    isPart[i][j] = true;
+                }
+                if(isPart[i][j]) {
+                    double thresh = 0.05;
+
+                    int vert = 0;
+                    int horz = 0;
+
+                    if(macroVel[i][j][0] > thresh){
+                        horz = 1;
+                    }else if(macroVel[i][j][0] < -thresh){
+                        horz = -1;
+                    }
+
+                    if(macroVel[i][j][1] > thresh){
+                        vert = 1;
+                    }else if(macroVel[i][j][1] < -thresh){
+                        vert = -1;
+                    }
+
+                    if(i + horz >= 0 && i + horz < width && j + vert >= 0 && j + vert < height){
+                        nextPart[i + horz][j + vert] = true;
+                    }
+
+//                    System.out.println(maxInd + "     "  +max);
+                }
+            }
+        }
+        isPart = nextPart;
+    }
+
     public void step () {
         stream2();
-//        updateMD();
-//        updateMV();
-//        updateED();
         updateMDMVED();
         collide();
         clampAll();
+        moveParts();
     }
 
     @Override
@@ -403,21 +465,29 @@ public class Lattice extends JPanel {
 
                 int val = (int) clamp(0,255,Math.abs(macroDense[i][j]*denseMult)); // density
                 int val1 = (int) clamp(0,255,Math.abs(macroVel[i][j][0]*velMult)); // horizontal speed
+                double val1d = clamp(0,255,Math.abs(macroVel[i][j][0]*velMult)); // horizontal speed
                 int val2 = (int) clamp(0,255,Math.abs(macroVel[i][j][1]*velMult)); // vertical speed
+                double val2d = clamp(0,255,Math.abs(macroVel[i][j][1]*velMult)); // vertical speed
                 int val3 = (int) clamp(0,255, val1+val2); // total speed
-                int val3half = (int) clamp(0,255, val1*val1 + val2*val2); // total speed
+                int val3half = (int) clamp(0,255, (val1d*val1d + val2d*val2d)/sqrMult); // total speed
                 int val4 = (int) clamp(0,255,macroVel[i][j][0]*velMult); // positive horizontal
                 int val5 = (int) clamp(0,255,-macroVel[i][j][0]*velMult); // negative horizontal
                 int val6 = (int) clamp(0,255,macroVel[i][j][1]*velMult); // positive vert
                 int val7 = (int) clamp(0,255,-macroVel[i][j][1]*velMult); // negative vert
+                int val8 = (int) clamp(0,255,isPart[i][j]?partTop:partBot); // has particle?
 
                 if(isWall[i][j]){
                     g2d.setColor(new Color(100, 100, 100));
                 }else {
+                    if(curCol == color.denseVel) {
+                        g2d.setColor(Color.getHSBColor(0.5f - (val3half / 512f), 1f, val / 255f)); // hue for speed, value for density
+                    }else if(curCol == color.partVel) {
+                        g2d.setColor(Color.getHSBColor(0.5f - (val3half / 512f), 1f, val8 / 255f)); // hue for speed, value for particle
+                    }else {
 //                    g2d.setColor(new Color(val3, val, 255-val3)); // speed + dense
 //                    g2d.setColor(new Color(val, val, val)); // all density
-                    g2d.setColor(Color.getHSBColor(0.5f-(val3half/512f), 1f, val/255f)); // hue for speed, value for density
 //                    g2d.setColor(new Color(val6, val4/4, val7)); // speed
+                    }
                 }
                 g2d.drawLine(i,j,i,j);
 
--- src/graphicsDriver.java
+++ src/graphicsDriver.java
@@ -50,33 +50,33 @@ public class graphicsDriver extends JFrame {
     }
 
     public static void main(String[] args) {
-        int frameWidth = 500; // Poiseuille flow
-        int frameHeight = 200;
+//        int frameWidth = 500; // Poiseuille flow
+//        int frameHeight = 200;
 
-//        int frameWidth = 256; // moving lid
-//        int frameHeight = 256;
+        int frameWidth = 256; // moving lid
+        int frameHeight = 256;
 
         frame = new javax.swing.JFrame();
-        frame.setSize(frameWidth, frameHeight+100);
+        frame.setSize(frameWidth+100, frameHeight+100);
         frame.setLayout(new BorderLayout());
 
-        lat = new Lattice(frameWidth, frameHeight);
+        lat = new Lattice(frameWidth, frameHeight, Lattice.setup.MoveLid, Lattice.color.denseVel);
 
         // ~~~~~~~~~~~~~~~~    Poiseuille flow ~~~~~~~~~~~~~~~~
 
-        for (int i = 2; i < lat.height-2; i++) {
-            lat.zouHe[1][i] = 3;
-            lat.zouHeOr[1][i] = 1;
-            lat.zhVel[1][i][0] = 1d/2;
-            lat.zhPres[1][i] = 1;
-        }
-
-        for (int i = 2; i < lat.height-2; i++) {
-            lat.zouHe[lat.width-2][i] = 1;
-            lat.zouHeOr[lat.width-2][i] = 3;
-//                lat.zhVel[lat.width-2][i][0] = -1d/7;
-            lat.zhPres[lat.width-2][i] = -0;
-        }
+//        for (int i = 2; i < lat.height-2; i++) {
+//            lat.zouHe[1][i] = 3;
+//            lat.zouHeOr[1][i] = 1;
+//            lat.zhVel[1][i][0] = 1d/2;
+//            lat.zhPres[1][i] = 1;
+//        }
+//
+//        for (int i = 2; i < lat.height-2; i++) {
+//            lat.zouHe[lat.width-2][i] = 1;
+//            lat.zouHeOr[lat.width-2][i] = 3;
+////                lat.zhVel[lat.width-2][i][0] = -1d/7;
+//            lat.zhPres[lat.width-2][i] = -0;
+//        }
 
         // ~~~~~~~~~~~~~~~~ moving lid ~~~~~~~~~~~~~~~~
 
@@ -93,7 +93,16 @@ public class graphicsDriver extends JFrame {
 
         JButton run = new JButton("run");
         JButton pause = new JButton("pause");
-        JButton reset = new JButton("reset");
+        JButton resetWalls = new JButton("reset walls");
+        JButton resetFlow = new JButton("reset flow");
+        JButton resetAll = new JButton("reset all");
+
+        JPanel colPan = new JPanel();
+        colPan.setLayout(new GridLayout(0,1));
+        colPan.setPreferredSize(new Dimension(100, 10));
+
+        JButton denseVel = new JButton("Dense + Vel");
+        JButton partVel = new JButton("Part + Vel");
 
         JLabel info = new JLabel("Dense: 0    |     Vel: 0,0", JLabel.CENTER);
         info.setText("tmp");
@@ -111,14 +120,44 @@ public class graphicsDriver extends JFrame {
             run.setEnabled(true);
             pause.setEnabled(false);
         });
-        reset.addActionListener(e -> {
+        resetWalls.addActionListener(e -> {
             running = false;
             run.setEnabled(true);
             pause.setEnabled(false);
 
-            lat.reset();
+            lat.resetWalls();
             frame.revalidate();
             frame.repaint();
+        });
+        resetFlow.addActionListener(e -> {
+            running = false;
+            run.setEnabled(true);
+            pause.setEnabled(false);
+
+            lat.resetFlow();
+            frame.revalidate();
+            frame.repaint();
+        });
+        resetAll.addActionListener(e -> {
+            running = false;
+            run.setEnabled(true);
+            pause.setEnabled(false);
+
+            lat.resetWalls();
+            lat.resetFlow();
+            frame.revalidate();
+            frame.repaint();
+        });
+
+        denseVel.addActionListener(e -> {
+            lat.curCol = Lattice.color.denseVel;
+            denseVel.setEnabled(false);
+            partVel.setEnabled(true);
+        });
+        partVel.addActionListener(e -> {
+            lat.curCol = Lattice.color.partVel;
+            denseVel.setEnabled(true);
+            partVel.setEnabled(false);
         });
 
         lat.addMouseMotionListener(new MouseMotionListener() {
@@ -128,22 +167,15 @@ public class graphicsDriver extends JFrame {
 
                 for (int i = -1; i <= 1; i++) {
                     for (int j = -1; j < 1; j++) {
-                        if(e.getX()+i >= 0 && e.getX()+i < lat.width && e.getY()+j >= 0 && e.getY()+j < lat.height)
-                        lat.isWall[e.getX()+i][e.getY()+j] = SwingUtilities.isLeftMouseButton(e);
-//                                e.getButton() == MouseEvent.BUTTON1;
+                        if(e.getX()+i >= 0 && e.getX()+i < lat.width && e.getY()+j >= 0 && e.getY()+j < lat.height) {
+                            if(lat.curCol == Lattice.color.denseVel){
+                                lat.isWall[e.getX() + i][e.getY() + j] = SwingUtilities.isLeftMouseButton(e);
+                            }else if(lat.curCol == Lattice.color.partVel){
+                                lat.isPart[e.getX() + i][e.getY() + j] = SwingUtilities.isLeftMouseButton(e);
+                            }
+                        }
                     }
                 }
-
-//                lat.isWall[e.getX()][e.getY()] = e.getButton() == MouseEvent.BUTTON1;
-//
-//                lat.isWall[e.getX()-1][e.getY()] = e.getButton() == MouseEvent.BUTTON1;
-//                lat.isWall[e.getX()-1][e.getY()-1] = e.getButton() == MouseEvent.BUTTON1;
-//                lat.isWall[e.getX()][e.getY()-1] = e.getButton() == MouseEvent.BUTTON1;
-//                lat.isWall[e.getX()+1][e.getY()-1] = e.getButton() == MouseEvent.BUTTON1;
-//                lat.isWall[e.getX()+1][e.getY()] = e.getButton() == MouseEvent.BUTTON1;
-//                lat.isWall[e.getX()+1][e.getY()+1] = e.getButton() == MouseEvent.BUTTON1;
-//                lat.isWall[e.getX()][e.getY()+1] = e.getButton() == MouseEvent.BUTTON1;
-//                lat.isWall[e.getX()-1][e.getY()+1] = e.getButton() == MouseEvent.BUTTON1;
 
                 frame.revalidate();
                 frame.repaint();
@@ -153,24 +185,29 @@ public class graphicsDriver extends JFrame {
             public void mouseMoved(MouseEvent e) {
                 if(e.getX() >= 0 && e.getX() < lat.width && e.getY() >= 0 && e.getY() < lat.height)
                     info.setText("Dense: " + lat.macroDense[e.getX()][e.getY()] + "    |     Vel: " +
-                            lat.macroVel[e.getX()][e.getY()][0] + "," + lat.macroVel[e.getX()][e.getY()][1]);
+                            lat.macroVel[e.getX()][e.getY()][0] + "," + lat.macroVel[e.getX()][e.getY()][1] + "    |     Part: " +
+                            (lat.isPart[e.getX()][e.getY()]?"yes":"no"));
             }
         });
 
 
         controlPanel.add(run);
         controlPanel.add(pause);
-        controlPanel.add(reset);
+        controlPanel.add(resetWalls);
+        controlPanel.add(resetFlow);
+        controlPanel.add(resetAll);
+
+        colPan.add(denseVel);
+        colPan.add(partVel);
 
         frame.add(controlPanel, BorderLayout.NORTH);
+        frame.add(colPan, BorderLayout.EAST);
         frame.add(lat, BorderLayout.CENTER);
         frame.add(info, BorderLayout.SOUTH);
 
         frame.setVisible(true);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-//        update ud = new update();
 
         Timer timer = new Timer();
         timer.schedule(new update(), 0, 1);
